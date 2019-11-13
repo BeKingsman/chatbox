0eca8733921cab30476a767253b4e4d51b612d17
HARSH AGARWAL <harshagarwal8055@gmail.com>
2019-11-13 20:53:28 +0530

    message form , view to display form and 3 templates added.

--- chats/models.py
+++ chats/models.py
@@ -7,6 +7,6 @@ from datetime import datetime
 
 class Message(models.Model):
     content = models.TextField()
-    sent_by = models.ForeignKey(User, on_delete=models.CASCADE,related_name='kisne_bheja')
-    m_sent_to = models.ForeignKey(User, on_delete=models.CASCADE,related_name='kisko_bheja')
+    sent_by = models.ForeignKey(User, on_delete=models.CASCADE,related_name='kisne_bheja',null = True)
+    m_sent_to = models.ForeignKey(User, on_delete=models.CASCADE,related_name='kisko_bheja',null = True)
     time = models.DateTimeField(default=datetime.now, blank=False)
--- chats/views.py
+++ chats/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .forms import *
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse('<h1> Chat Home </h1>')
+    users = User.objects.all()
+    
+    return render(request,'home.html',{'users':users})
+
+
+
+def Chat(request,pk):
+    if(request.method == 'POST'):
+        form = Messageform(request.POST)
+        form.instance.m_sent_to = User.objects.get(pk=pk)
+        form.instance.sent_by = request.user
+        if (form.is_valid):
+            form.save()
+        
+    form = Messageform()
+    
+    return render(request,'message.html',{'form':form})
