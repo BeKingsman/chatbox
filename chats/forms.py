efd706afe0e03b735c6c0a8276213fe4c82aec3d
Ayush <ayush.jan10@gmail.com>
2019-11-13 23:51:46 +0530

    Added login and register

--- chats/forms.py
+++ chats/forms.py
@@ -1,6 +1,7 @@
 from .models import *
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class Messageform(forms.ModelForm):
@@ -10,8 +11,15 @@ class Messageform(forms.ModelForm):
         fields = ['content']
 
 
-class UserRegisterForm(forms.ModelForm):
+# class UserRegisterForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', ]
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', ]
+        fields = ['username', 'email', 'password1', 'password2']
--- chats/views.py
+++ chats/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import *
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -12,12 +14,13 @@ def homepage(request):
 
 
 def Chat(request, pk):
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         form = Messageform(request.POST)
         form.instance.m_sent_to = User.objects.get(pk=pk)
         form.instance.sent_by = request.user
-        if (form.is_valid):
+        if form.is_valid:
             form.save()
+            return redirect('home')
 
     form = Messageform()
 
@@ -25,14 +28,25 @@ def Chat(request, pk):
 
 
 def user_login(request):
-    pass
+
+    if request.method == "POST":
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        login(request, user)
+        return redirect('home')
+    return render(request, 'login.html')
 
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
+            return redirect('login')
 
     form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
